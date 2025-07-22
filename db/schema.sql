d17887ba569f12c51206b4b21dcbd1b8c7c0c84e
Chris John <chrisgachuhi@gmail.com>
2025-07-22 19:25:01 +0300

    Created Schema | added users table

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+-- USERS
+CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    role VARCHAR(50) NOT NULL CHECK(role('client', 'freelancer', 'admin')), 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+)
+
+
+
+-- JOBS
+-- PROPOSALS
+-- MESSAGES
