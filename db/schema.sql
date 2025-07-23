5b675633a6e8fed47f7280502ba5f6d8d7584fdd
Chris John <chrisgachuhi@gmail.com>
2025-07-23 19:05:30 +0300

    Created Schema | added jobs,proposals,messages table

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY, --we use SERIAL so that it increments ids automatically
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    role VARCHAR(20) NOT NULL CHECK(role('client', 'freelancer', 'admin')), 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    role VARCHAR(20) NOT NULL CHECK(role IN ('client', 'freelancer', 'admin')), 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 
 -- JOBS
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS jobs(
     client_id INTEGER NOT NULL REFERENCES users(id) on DELETE CASCADE, --Deletes child rows if parent is deleted
     title VARCHAR(200) NOT NULL,
     description TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 
 -- PROPOSALS
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS proposals(
     job_id INTEGER NOT NULL REFERENCES jobs(id) on DELETE CASCADE,
     freelancer_id INTEGER NOT NULL REFERENCES users(id) on DELETE CASCADE,
     cover_letter TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 
 -- MESSAGES
@@ -32,5 +32,5 @@ CREATE TABLE IF NOT EXISTS messages(
     sender_id INTEGER NOT NULL REFERENCES users(id) on DELETE CASCADE,
     receiver_id INTEGER NOT NULL REFERENCES users(id) on DELETE CASCADE,
     content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
